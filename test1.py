aed913e6d2d296caf4befc64f44d5dbceae371a0
lifu <lifu5353@gmail.com>
2022-06-05 20:50:59 +0800

    first commit

---
+++ test1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+   File Name:  test1.py
+   Author :    lifu
+   Time:       2022/6/5 20:42
+"""
+
+print("Hello world")
---
+++ utils/plot.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+   File Name:  plot.py
+   Author :    lifu
+   Time:       2022/6/5 20:45
+"""
+
+print("Plot a illustration")
